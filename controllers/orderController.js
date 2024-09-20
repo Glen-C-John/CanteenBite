a5371fdebe4c53072fe885c2730ddf1012c35986
12Joseph86 <josephanthony200485@gmail.com>
2024-09-20 17:17:42 +0530

    final

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -7,7 +7,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
 //placing user order from frontend
 const placeOrder= async(req,res) => {
 
-    const frontend_url = "http://localhost:5173"
+    const frontend_url = "http://localhost:5173"     //update frontend url
     try{
         const neworder = new orderModel({
             userId:req.body.userId,
@@ -51,8 +51,58 @@ const placeOrder= async(req,res) => {
 
     }
     catch(error){
-
+        console.log(error);
+        res.json({success:false,message:"error"})
     }
 }
 
-export {placeOrder}
+const verifyOrder = async(req,res)=>{
+    const {orderId,success}=req.body;
+    try{
+        if(success=="true"){
+            await orderModel.findByIdAndUpdate(orderId,{payment:true});
+            res.json({success:true,message:"paid"})
+        }
+        else{
+            await orderModel.findByIdAndDelete(orderId);
+            res.json({succes:"false",message:"not paid"})
+        }
+    
+    }catch(error){
+            console.log(error)
+            res.json({success:false,message:"error"})
+        }
+    }
+
+    //user orders for frontend
+    const userOrders = async (req,res) => {
+        try{
+            const orders = await orderModel.find({userId:req.body.userId})
+            res.json({success:true,data:orders})
+        }catch(error){
+            console.log(error)
+            res.json({success:false,message:"Error"})
+        }
+    }
+//listing orders fro admin panel
+const listOrders = async (req,res) => {
+    try{
+        const orders = await orderModel.find({});
+        res.json({success:true,data:orders})
+    }catch(error){
+        console.log(error)
+        res.json({success:false,message:error})
+    }
+}
+
+//api for updating order status
+const updateStatus= async (req,res) =>{
+    try{
+        await orderModel.findByIdAndUpdate(req.body.orderId,{status:req.body.status})
+        res.json({success:true,message:"status Updated"})
+    }catch(error){
+        console.log(error);
+        res.json({success:false,message:"error"})
+    }
+}
+export {placeOrder,verifyOrder,userOrders,listOrders,updateStatus}
