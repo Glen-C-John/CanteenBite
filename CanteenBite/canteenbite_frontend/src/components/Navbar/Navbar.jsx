09e182218a3a3dd780fca3a20a8e667133eb3c4d
Glen-C-John <144381784+Glen-C-John@users.noreply.github.com>
2026-05-05 16:37:23 +0530

    updated navbar

--- CanteenBite/canteenbite_frontend/src/components/Navbar/Navbar.jsx
+++ CanteenBite/canteenbite_frontend/src/components/Navbar/Navbar.jsx
@@ -26,7 +26,7 @@ const Navbar = ({setShowLogin}) => {
         <Link to='/' onClick={()=>setMenu ("home" )} className={menu==="home" ?"active": ""}>Home</Link>
         <a href="#explore-menu" onClick={()=>setMenu ("menu" )} className={menu==="menu" ?"active": ""}>Menu</a>
         <a href="#footer" onClick={()=>setMenu ("contact-us" )} className={menu==="contact-us" ?"active": ""}>contact us</a>
-        <li onClick={()=>setMenu ("mobile-app" )} className={menu==="mobile-app" ?"active": ""}></li>
+        <a href="#app-download" onClick={()=>setMenu ("mobile-app" )} className={menu==="mobile-app" ?"active": ""}>mobile app</a>
       </ul>
       <div className='navbar-right'>
         <img src={assets.search_icon} alt="" />
@@ -39,7 +39,7 @@ const Navbar = ({setShowLogin}) => {
         :<div className='navbar-profile'>
           <img src={assets.profile_icon} alt="" />
           <ul className="navbar-profile-dropdown">
-            <li><img src={assets.bag_icon} alt="" /><p>Orders</p></li>
+            <li onClick={() => navigate('/myorders')}><img src={assets.bag_icon} alt="" /><p>Orders</p></li>
             <hr />
             <li onClick={logout}><img src={assets.logout_icon} alt="" /><p>Logout</p></li>
           </ul>
