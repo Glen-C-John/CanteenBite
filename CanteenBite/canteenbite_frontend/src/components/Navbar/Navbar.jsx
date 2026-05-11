0173adbcaf599f7ceae4f16935dd09ad7bde1e87
Glen-C-John <144381784+Glen-C-John@users.noreply.github.com>
2026-05-12 02:02:59 +0530

    updated navbar

--- CanteenBite/canteenbite_frontend/src/components/Navbar/Navbar.jsx
+++ CanteenBite/canteenbite_frontend/src/components/Navbar/Navbar.jsx
@@ -24,9 +24,9 @@ const Navbar = ({setShowLogin}) => {
         <Link to='/'><h1>CanteenBites</h1></Link>
       <ul className='navbar-menu'>
         <Link to='/' onClick={()=>setMenu ("home" )} className={menu==="home" ?"active": ""}>Home</Link>
-        <a href="#explore-menu" onClick={()=>setMenu ("menu" )} className={menu==="menu" ?"active": ""}>Menu</a>
-        <a href="#footer" onClick={()=>setMenu ("contact-us" )} className={menu==="contact-us" ?"active": ""}>contact us</a>
-        <a href="#app-download" onClick={()=>setMenu ("mobile-app" )} className={menu==="mobile-app" ?"active": ""}>mobile app</a>
+        <a href="/#explore-menu" onClick={()=>setMenu ("menu" )} className={menu==="menu" ?"active": ""}>Menu</a>
+        <a href="/#footer" onClick={()=>setMenu ("contact-us" )} className={menu==="contact-us" ?"active": ""}>contact us</a>
+        {token ? <Link to="/myorders" onClick={()=>setMenu("myorders")} className={menu==="myorders"?"active":""}>My Orders</Link> : null}
       </ul>
       <div className='navbar-right'>
         <img src={assets.search_icon} alt="" />
