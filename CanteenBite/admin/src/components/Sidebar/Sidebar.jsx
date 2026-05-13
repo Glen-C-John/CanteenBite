af2fa5bce64ec78720b85a374baabc682748f72f
Glen-C-John <144381784+Glen-C-John@users.noreply.github.com>
2026-05-13 21:30:34 +0530

    updated sidebar.jsx

--- CanteenBite/admin/src/components/Sidebar/Sidebar.jsx
+++ CanteenBite/admin/src/components/Sidebar/Sidebar.jsx
@@ -2,23 +2,30 @@ import React from 'react'
 import'./Sidebar.css'
 import { assets } from '../../assets/assets'
 import { NavLink } from 'react-router-dom'
+import { motion } from 'framer-motion'
 
 const Sidebar = () => {
   return (
     <div className='sidebar'>
         <div className="sidebar-options">
-            <NavLink to='/add' className="sidebar-option">
-                <img src={assets.add_icon} alt="" />
-                <p>Add Items</p>
-            </NavLink>
-            <NavLink to='/list' className="sidebar-option">
-                <img src={assets.order_icon} alt="" />
-                <p>List Items</p>
-            </NavLink>
-            <NavLink to='orders' className="sidebar-option">
-                <img src={assets.order_icon} alt="" />
-                <p>Orders</p>
-            </NavLink>
+            <motion.div initial={{opacity: 0, x: -20}} animate={{opacity: 1, x: 0}} transition={{duration: 0.3, delay: 0.1}}>
+                <NavLink to='/add' className="sidebar-option">
+                    <img src={assets.add_icon} alt="" />
+                    <p>Add Items</p>
+                </NavLink>
+            </motion.div>
+            <motion.div initial={{opacity: 0, x: -20}} animate={{opacity: 1, x: 0}} transition={{duration: 0.3, delay: 0.2}}>
+                <NavLink to='/list' className="sidebar-option">
+                    <img src={assets.order_icon} alt="" />
+                    <p>List Items</p>
+                </NavLink>
+            </motion.div>
+            <motion.div initial={{opacity: 0, x: -20}} animate={{opacity: 1, x: 0}} transition={{duration: 0.3, delay: 0.3}}>
+                <NavLink to='orders' className="sidebar-option">
+                    <img src={assets.order_icon} alt="" />
+                    <p>Orders</p>
+                </NavLink>
+            </motion.div>
         </div>
       
     </div>
