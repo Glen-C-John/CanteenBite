f6aa3c585904ba8f9750c8e2212767ceadf143a8
Glen-C-John <144381784+Glen-C-John@users.noreply.github.com>
2026-05-13 21:29:48 +0530

    updated navbar.jsx

--- CanteenBite/admin/src/components/Navbar/Navbar.jsx
+++ CanteenBite/admin/src/components/Navbar/Navbar.jsx
@@ -1,14 +1,20 @@
 import React from 'react'
 import './Navbar.css'
 import {assets} from '../../assets/assets'
+import { motion } from 'framer-motion'
 
 const Navbar = () => {
   return (
-    <div className='navbar'>
+    <motion.div 
+      className='navbar'
+      initial={{ opacity: 0, y: -20 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.5 }}
+    >
         <h1>CanteenBites</h1>
         <img  className='profile' src={assets.profile_image} alt="" />
       
-    </div>
+    </motion.div>
   )
 }
 
