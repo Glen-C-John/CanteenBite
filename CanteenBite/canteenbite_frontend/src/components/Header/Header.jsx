6ae8ed03f07618a2698012c019310ad250812c59
Glen-C-John <144381784+Glen-C-John@users.noreply.github.com>
2026-05-20 23:50:14 +0530

    updated header

--- CanteenBite/canteenbite_frontend/src/components/Header/Header.jsx
+++ CanteenBite/canteenbite_frontend/src/components/Header/Header.jsx
@@ -1,14 +1,23 @@
 import React from 'react'
 import './Header.css'
+import { motion } from 'framer-motion'
 
 const Header = () => {
+
   return (
     <div className='header'>
-        <div className="header-contents">
+        <motion.div 
+            className="header-contents"
+            initial={{ opacity: 0, y: 50 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.8, ease: "easeOut" }}
+        >
             <h2>Order your favourite food here</h2>
             <p>Choose from a diverse menu featuring a delectable array of dishes crafted with the finest ingredients and culinary expertise. Our mission is is to satisfy your cravings and elevate your dining experience, one delicious meal at a time.</p>
-            {/* <button>View Menu</button> */}
-        </div>
+            <div>
+              <a href="/#explore-menu"><button>View Menu</button></a>
+            </div>
+        </motion.div>
     </div>
   )
 }
