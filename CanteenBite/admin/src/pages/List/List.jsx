b32276cb6a43c3899da42fca6dbcc999a529fa56
Glen-C-John <144381784+Glen-C-John@users.noreply.github.com>
2026-05-20 23:51:16 +0530

    updated list

--- CanteenBite/admin/src/pages/List/List.jsx
+++ CanteenBite/admin/src/pages/List/List.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import './List.css'
 import axios from 'axios';
 import { toast } from 'react-toastify';
+import { motion } from 'framer-motion';
 
 const List = ({url}) => {
 
@@ -46,13 +47,19 @@ const List = ({url}) => {
         </div>
         {list.map((item,index)=>{
             return (
-              <div key={index} className='list-table-format'>
+              <motion.div 
+                key={index} 
+                className='list-table-format'
+                initial={{ opacity: 0, y: 20 }}
+                animate={{ opacity: 1, y: 0 }}
+                transition={{ duration: 0.4, delay: index * 0.05 }}
+              >
               <img src={`${url}/images/`+item.image} alt="" />
               <p>{item.name}</p>
               <p>{item.category}</p>
               <p>₹{item.price}</p>
               <p onClick={()=>removeFood(item._id)} className='cursor'>X</p>
-              </div>
+              </motion.div>
             )
         })}
       </div>
