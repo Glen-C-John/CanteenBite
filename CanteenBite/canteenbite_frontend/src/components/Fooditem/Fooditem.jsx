2dfec6e95f8ad3ec69fa0e74c66e8c1b0d0c3aed
Glen-C-John <144381784+Glen-C-John@users.noreply.github.com>
2026-06-11 23:38:48 +0530

    updated fooditems

--- CanteenBite/canteenbite_frontend/src/components/Fooditem/Fooditem.jsx
+++ CanteenBite/canteenbite_frontend/src/components/Fooditem/Fooditem.jsx
@@ -5,20 +5,25 @@ import { StoreContext } from '../../context/StoreContext'
 
 const Fooditem = ({id,name,price,description,image}) => {
 
-    const {cartItems,addToCart,removeFromCart,url} = useContext(StoreContext);
+    const {cartItems,addToCart,removeFromCart,url,food_list} = useContext(StoreContext);
+    const item = food_list.find(f => f._id === id);
+    const isAvailable = item ? item.available !== false : true;
 
   return (
-    <div className='food-item'>
+    <div className={`food-item ${!isAvailable ? 'sold-out' : ''}`}>
         <div className="food-item-img-container">
             <img className='food-item-image' src={url+"/images/"+image} alt="" />
-            {!cartItems[id]
-                ?<img className='add' onClick={()=>addToCart(id)} src={assets.add_icon_white} alt="" />
-                :<div className="food-item-counter">
-                  <img onClick={()=>removeFromCart(id)} src={assets.remove_icon_red} alt="" />
-                  <p>{cartItems[id]}</p>
-                  <img onClick={()=>addToCart(id)} src={assets.add_icon_green} alt="" />
-                </div>
-            }
+            {!isAvailable ? (
+                <div className="sold-out-badge">Sold Out</div>
+            ) : (
+                !cartItems[id]
+                    ?<img className='add' onClick={()=>addToCart(id)} src={assets.add_icon_white} alt="" />
+                    :<div className="food-item-counter">
+                      <img onClick={()=>removeFromCart(id)} src={assets.remove_icon_red} alt="" />
+                      <p>{cartItems[id]}</p>
+                      <img onClick={()=>addToCart(id)} src={assets.add_icon_green} alt="" />
+                    </div>
+            )}
         </div>
         <div className="food-item-info">
             <div className="food-item-name-rating">
